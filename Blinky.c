128b4d5eaeb6c43dae4a6f2b0209493d09b5fc56
Christopher Seidl <KeilChris@users.noreply.github.com>
2021-02-04 08:59:48 +0100

    Changed to new Blinky

--- Blinky.c
+++ Blinky.c
@@ -1,144 +1,165 @@
-/*----------------------------------------------------------------------------
- *      RL-ARM - RTX
- *----------------------------------------------------------------------------
- *      Name:    BLinky.c
+/* -------------------------------------------------------------------------- 
+ * Copyright (c) 2013-2019 ARM Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *      Name:    Blinky.c
  *      Purpose: RTX example program
- *----------------------------------------------------------------------------
- *      This code is part of the RealView Run-Time Library.
- *      Copyright (c) 2004-2014 KEIL - An ARM Company. All rights reserved.
+ *
  *---------------------------------------------------------------------------*/
 
-// test to commit
-// another
+#include <stdio.h>
 
+#include "cmsis_os2.h"                  // ARM::CMSIS:RTOS2:Keil RTX5
 
-#include "cmsis_os.h"
-#include "LPC18xx.h"
-#include "Board_LED.h"
+#include "RTE_Components.h"
+#include CMSIS_device_header
 
-osThreadId tid_phaseA;                  /* Thread id of thread: phase_a      */
-osThreadId tid_phaseB;                  /* Thread id of thread: phase_b      */
-osThreadId tid_phaseC;                  /* Thread id of thread: phase_c      */
-osThreadId tid_phaseD;                  /* Thread id of thread: phase_d      */
-osThreadId tid_clock;                   /* Thread id of thread: clock        */
-osThreadId tid_lcd;                     /* Thread id of thread: lcd          */
+osThreadId_t tid_phaseA;                /* Thread id of thread: phase_a      */
+osThreadId_t tid_phaseB;                /* Thread id of thread: phase_b      */
+osThreadId_t tid_phaseC;                /* Thread id of thread: phase_c      */
+osThreadId_t tid_phaseD;                /* Thread id of thread: phase_d      */
+osThreadId_t tid_clock;                 /* Thread id of thread: clock        */
 
-#define LED_A   0
-#define LED_B   1
-#define LED_C   2
-#define LED_D   3
-#define LED_CLK 7
+struct phases_t {
+  int_fast8_t phaseA;
+  int_fast8_t phaseB;
+  int_fast8_t phaseC;
+  int_fast8_t phaseD;
+} g_phases;
 
 
 /*----------------------------------------------------------------------------
  *      Switch LED on
  *---------------------------------------------------------------------------*/
 void Switch_On (unsigned char led) {
-  LED_On(led);
+  printf("LED On:  #%d\n\r", led);
 }
 
 /*----------------------------------------------------------------------------
  *      Switch LED off
  *---------------------------------------------------------------------------*/
 void Switch_Off (unsigned char led) {
-  LED_Off(led);
+  printf("LED Off: #%d\n\r", led);
 }
 
 
 /*----------------------------------------------------------------------------
  *      Function 'signal_func' called from multiple threads
  *---------------------------------------------------------------------------*/
-void signal_func (osThreadId tid)  {
-  osSignalSet(tid_clock, 0x0100);           /* set signal to clock thread    */
+void signal_func (osThreadId_t tid)  {
+  osThreadFlagsSet(tid_clock, 0x0100);      /* set signal to clock thread    */
   osDelay(500);                             /* delay 500ms                   */
-  osSignalSet(tid_clock, 0x0100);           /* set signal to clock thread    */
+  osThreadFlagsSet(tid_clock, 0x0100);      /* set signal to clock thread    */
   osDelay(500);                             /* delay 500ms                   */
-  osSignalSet(tid, 0x0001);                 /* set signal to thread 'thread' */
+  osThreadFlagsSet(tid, 0x0001);            /* set signal to thread 'thread' */
   osDelay(500);                             /* delay 500ms                   */
 }
 
 /*----------------------------------------------------------------------------
  *      Thread 1 'phaseA': Phase A output
  *---------------------------------------------------------------------------*/
-void phaseA (void const *argument) {
+void phaseA (void *argument) {
   for (;;) {
-    osSignalWait(0x0001, osWaitForever);    /* wait for an event flag 0x0001 */
-    Switch_On (LED_A);
-    signal_func(tid_phaseB);                /* call common signal function   */
-    Switch_Off(LED_A);
+    osThreadFlagsWait(0x0001, osFlagsWaitAny ,osWaitForever);    /* wait for an event flag 0x0001 */
+    Switch_On(0);
+    g_phases.phaseA = 1;
+    signal_func(tid_phaseB);                                     /* call common signal function   */
+    g_phases.phaseA = 0;
+    Switch_Off(0);
   }
 }
 
 /*----------------------------------------------------------------------------
  *      Thread 2 'phaseB': Phase B output
  *---------------------------------------------------------------------------*/
-void phaseB (void const *argument) {
+void phaseB (void *argument) {
   for (;;) {
-    osSignalWait(0x0001, osWaitForever);    /* wait for an event flag 0x0001 */
-    Switch_On (LED_B);
+    osThreadFlagsWait(0x0001, osFlagsWaitAny, osWaitForever);    /* wait for an event flag 0x0001 */
+    Switch_On(1);
+    g_phases.phaseB = 1;
     signal_func(tid_phaseC);                /* call common signal function   */
-    Switch_Off(LED_B);
+    g_phases.phaseB = 0;
+    Switch_Off(1);
   }
 }
 
 /*----------------------------------------------------------------------------
  *      Thread 3 'phaseC': Phase C output
  *---------------------------------------------------------------------------*/
-void phaseC (void const *argument) {
+void phaseC (void *argument) {
   for (;;) {
-    osSignalWait(0x0001, osWaitForever);    /* wait for an event flag 0x0001 */
-    Switch_On (LED_C);
+    osThreadFlagsWait(0x0001, osFlagsWaitAny, osWaitForever);    /* wait for an event flag 0x0001 */
+    Switch_On(2);
+    g_phases.phaseC = 1;
     signal_func(tid_phaseD);                /* call common signal function   */
-    Switch_Off(LED_C);
+    g_phases.phaseC = 0;
+    Switch_Off(2);
   }
 }
 
 /*----------------------------------------------------------------------------
  *      Thread 4 'phaseD': Phase D output
  *---------------------------------------------------------------------------*/
-void phaseD (void  const *argument) {
+void phaseD (void *argument) {
   for (;;) {
-    osSignalWait(0x0001, osWaitForever);    /* wait for an event flag 0x0001 */
-    Switch_On (LED_D);
+    osThreadFlagsWait(0x0001, osFlagsWaitAny, osWaitForever);    /* wait for an event flag 0x0001 */
+    Switch_On(3);
+    g_phases.phaseD = 1;
     signal_func(tid_phaseA);                /* call common signal function   */
-    Switch_Off(LED_D);
+    g_phases.phaseD = 0;
+    Switch_Off(3);
   }
 }
 
 /*----------------------------------------------------------------------------
  *      Thread 5 'clock': Signal Clock
  *---------------------------------------------------------------------------*/
-void clock (void  const *argument) {
+void clock (void *argument) {
   for (;;) {
-    osSignalWait(0x0100, osWaitForever);    /* wait for an event flag 0x0100 */
-    Switch_On (LED_CLK);
+    osThreadFlagsWait(0x0100, osFlagsWaitAny, osWaitForever);    /* wait for an event flag 0x0100 */
     osDelay(80);                            /* delay  80ms                   */
-    Switch_Off(LED_CLK);
   }
 }
-
-
-
-osThreadDef(phaseA, osPriorityNormal, 1, 0);
-osThreadDef(phaseB, osPriorityNormal, 1, 0);
-osThreadDef(phaseC, osPriorityNormal, 1, 0);
-osThreadDef(phaseD, osPriorityNormal, 1, 0);
-osThreadDef(clock,  osPriorityNormal, 1, 0);
 
 /*----------------------------------------------------------------------------
  *      Main: Initialize and start RTX Kernel
  *---------------------------------------------------------------------------*/
-int main (void) {
+void app_main (void *argument) {
 
-  LED_Initialize();                         /* Initialize the LEDs           */
-  tid_phaseA = osThreadCreate(osThread(phaseA), NULL);
-  tid_phaseB = osThreadCreate(osThread(phaseB), NULL);
-  tid_phaseC = osThreadCreate(osThread(phaseC), NULL);
-  tid_phaseD = osThreadCreate(osThread(phaseD), NULL);
-  tid_clock  = osThreadCreate(osThread(clock),  NULL);
+  tid_phaseA = osThreadNew(phaseA, NULL, NULL);
+  tid_phaseB = osThreadNew(phaseB, NULL, NULL);
+  tid_phaseC = osThreadNew(phaseC, NULL, NULL);
+  tid_phaseD = osThreadNew(phaseD, NULL, NULL);
+  tid_clock  = osThreadNew(clock,  NULL, NULL);
 
-  osSignalSet(tid_phaseA, 0x0001);          /* set signal to phaseA thread   */
+  osThreadFlagsSet(tid_phaseA, 0x0001);     /* set signal to phaseA thread   */
 
   osDelay(osWaitForever);
+}
+
+int main (void) {
+
+  // System Initialization
+  SystemCoreClockUpdate();
+  // ...
+  osKernelInitialize();                 // Initialize CMSIS-RTOS
+  osThreadNew(app_main, NULL, NULL);    // Create application main thread
+  if (osKernelGetState() == osKernelReady) {
+    osKernelStart();                    // Start thread execution
+  }
+
   while(1);
 }
--- files/newfile.md
+++
@@ -1,9 +0,0 @@
-## New file as a small test
-
-### Heading 3 in mark-down
-
-- Let's add some content here!
-
-### Another h3 header
-
-- This is a subitem
